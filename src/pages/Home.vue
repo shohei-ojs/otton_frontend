90d119cbb919e5199abdcc084d9c7e5f4c6aa0f0
shohei-ojs <shohei.ojs@gmail.com>
2020-01-24 00:13:02 +0900

    add axios package

---
+++ src/pages/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <h1>{{ title }}</h1>
+    <button v-on:click=getHomePage>Hello</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  },
+  data: function () {
+      return {
+          title: 'HOME'
+      }
+  },
+  methods: {
+      getHomePage: function() {
+          this.title = "clicked"
+          this.axios.get('http://localhost:8081')
+          .then(response => this.title = response)
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
